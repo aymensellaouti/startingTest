cbc45b15971ad43659a6da0eb037e9b64aa5870a
aymen2782 <aymen.sellaouti@gmail.com>
2023-08-25 10:43:46 +0100

    fix bug

--- src/app/unitTest/mathService/math.service.spec.ts
+++ src/app/unitTest/mathService/math.service.spec.ts
@@ -1,7 +1,7 @@
 describe("MathService", () => {
-  it('should work', () => {
+  it("should work", () => {
     // Todo : Test if the service is well instanciated
-    const
+
     expect(1).toBe(1);
-  })
+  });
 });
