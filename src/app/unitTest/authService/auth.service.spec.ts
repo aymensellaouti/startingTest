f6f850458869eb9bc2a4667917b340129018fcf8
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-10 15:30:11 +0100

    exemple test authService

--- src/app/unitTest/authService/auth.service.spec.ts
+++ src/app/unitTest/authService/auth.service.spec.ts
@@ -1,5 +1,32 @@
-describe("AuthService", () => {
+import { AuthService } from "./auth.service";
+
+// LE describe c'est un groupement de test
+fdescribe('AuthService', () => {
   it('should work', () => {
+    // on implémente ici nos tests
     expect(1).toBe(1);
+  });
+  it('should return false when no user token in localStorage', () => {
+    // on implémente ici nos tests
+    // Arrange
+    localStorage.removeItem('user');
+    const authService = new AuthService();
+    const expectedResult = false;
+    //Act
+    const result = authService.isAuthentified();
+    //Assert
+    expect(expectedResult).toBe(result);
+  });
+
+  it('should return true when there is user token in localStorage', () => {
+    // on implémente ici nos tests
+    // Arrange
+    localStorage.setItem('user','test');
+    const authService = new AuthService();
+    const expectedResult = true;
+    //Act
+    const result = authService.isAuthentified();
+    //Assert
+    expect(expectedResult).toBe(result);
   });
 });
