8047940453b0a3c1bfa1de5ca6f804e2dafd96e8
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-01 09:38:50 +0100

    FIX BUG

--- src/app/unitTest/mathService/math.service.spec.ts
+++ src/app/unitTest/mathService/math.service.spec.ts
@@ -1,33 +1,14 @@
+import { LoggerService } from "src/app/services/logger.service";
 import { MathService } from "./math.service";
 import { TestBed } from "@angular/core/testing";
 
 let mathService: MathService;
+// let loggerService: LoggerService;
 fdescribe("MathService", () => {
-  beforeAll(() => {
-    // Arrange
-    //Instancier le mathService
-    mathService = TestBed.inject(MathService);
-  })
+  
   it('should work', () => { expect(1).toBe(1)})
-
   it('should add two numbers', () => {
-
-    // Act
-    // appeler la méthode add
-    const result = mathService.add(2,3);
-    const expectedResult = 5;
-    // Assert
-    // évaluer est ce que le résultat obtenu est égal au résultat attendu
-    expect(result).toEqual(expectedResult);
   })
   it('should substract two numbers', () => {
-
-    // Act
-    // appeler la méthode add
-    const result = mathService.substract(2,3);
-    const expectedResult = -1;
-    // Assert
-    // évaluer est ce que le résultat obtenu est égal au résultat attendu
-    expect(result).toEqual(expectedResult);
   })
 });
